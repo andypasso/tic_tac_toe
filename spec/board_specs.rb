5fd513e5bc101b571f570949e7bf9b11b16594ce
andynarf <untalandy@gmail.com>
2019-10-08 16:19:57 -0300

    board tests added

---
+++ spec/board_specs.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require_relative '../lib/color'
+
+require_relative '../lib/board'
+
+describe Board do
+  board = Board.new
+  board.cell[4] = 'X'.magenta
+
+  it 'returns nil if the cell available' do
+    expect(board.available?(2)).to be nil
+  end
+  it 'returns true if the cell is taken' do
+    expect(board.available?(5)).to be true
+  end
+end
